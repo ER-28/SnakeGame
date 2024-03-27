b37670154667e0b352717c42d91561c51eee89e2
l-guerra-rago <lylian.gr@protonmail.com>
2024-03-27 14:50:19 +0100

    WIP

--- SnakeGame/Menu.cs
+++ SnakeGame/Menu.cs
@@ -3,8 +3,8 @@ namespace SnakeGame;
 // display a menu with play and exit in center of console controllable with arrow keys and enter for selection
 public class Menu
 {
-    private bool open = true;
-    private bool selected;
+    private bool _open = true;
+    private bool _selected;
     
     public Menu()
     {
@@ -14,31 +14,31 @@ public class Menu
 
     private void Loop()
     {
-        selected = false;
-        while (open)
+        _selected = false;
+        while (_open)
         {
             Console.Clear();
             
             Console.SetCursorPosition(Console.WindowWidth / 2 - 4, Console.WindowHeight / 2 - 1);
-            Console.Write(selected ? "Play" : "> Play");
+            Console.Write(_selected ? "Play" : "> Play");
 
             Console.SetCursorPosition(Console.WindowWidth / 2 - 4, Console.WindowHeight / 2 + 1);
-            Console.Write(selected ? "> Exit" : "Exit");
+            Console.Write(_selected ? "> Exit" : "Exit");
 
             switch (Console.ReadKey().Key)
             {
                 case ConsoleKey.UpArrow:
-                    selected = false;
+                    _selected = false;
                     break;
                 case ConsoleKey.DownArrow:
-                    selected = true;
+                    _selected = true;
                     break;
                 case ConsoleKey.Enter:
-                    switch (selected)
+                    switch (_selected)
                     {
                         case false:
                             var game = new Game();
-                            open = false;
+                            _open = false;
                             break;
                         case true:
                             Environment.Exit(0);
